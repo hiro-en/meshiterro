b61a44fe59d2c8c88bea5d5113a9e12f3c5c5dd4
EC2 Default User <ec2-user@ip-172-31-17-155.ap-northeast-1.compute.internal>
2023-02-04 06:57:45 +0000

    <temporary　end>chapter 24

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,14 +1,25 @@
 class UsersController < ApplicationController
   def show
     @user = User.find(params[:id])
-    @post_images = @user.post_images
+    @post_images = @user.post_images.page(params[:page])
   end
 
   def edit
+    user_id = params[:id].to_i
+    unless user_id == current_user.id
+      redirect_to post_images_path
+    end
+    
+    
     @user = User.find(params[:id])
   end
   
   def update
+    user_id = params[:id].to_i
+    unless user_id == current_user.id
+      redirect_to post_images_path
+    end
+    
     @user = User.find(params[:id])
     @user.update(users_params)
     redirect_to user_path(@user.id)
